b230c37540c668e42de99fb9a4c7894cc72a7878
Imrie83 <czolgista83@gmail.com>
2022-01-03 17:18:33 +0100

    add models tests

--- donate_project/conftest.py
+++ donate_project/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from donate_app.models import Category
+from donate_app.models import Category, Institution, Donation
 
 
 @pytest.fixture
@@ -14,3 +14,15 @@ def create_categories():
         name='Category 3'
     )
     return [cat_1, cat_2, cat_3]
+
+
+@pytest.fixture
+def create_institute():
+    institute_1 = Institution.objects.update_or_create(
+        name='Inst 1',
+        type=1,
+    )
+    institute_2 = Institution.objects.update_or_create(
+        name='Inst 2',
+        type=2,
+    )
--- donate_project/tests/tests.py
+++ donate_project/tests/tests.py
@@ -1,8 +1,17 @@
 import pytest
 from donate_app.models import (
-    Category
+    Category,
+    Institution,
+    Donation,
 )
 
 @pytest.mark.django_db
 def test_category(client, create_categories):
     assert Category.objects.get(name='Category 1')
+    assert len(Category.objects.all()) == 3
+
+
+@pytest.mark.django_db
+def test_institute(client, create_institute):
+    assert Institution.objects.get(name='Inst 1')
+    assert len(Institution.objects.all()) == 2
