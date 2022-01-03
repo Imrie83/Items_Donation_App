19e154bed51010b268439338cdbee41f066c35e4
Imrie83 <czolgista83@gmail.com>
2022-01-03 13:16:44 +0100

    add Institution model.

--- donate/donate_app/models.py
+++ donate/donate_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+ORGANIZATIONS = [
+    (1, 'Fundacja'),
+    (2, 'Organizacja pozarządowa'),
+    (3, 'Zbiórka lokalna'),
+]
+
 
 class Category(models.Model):
     """
@@ -7,8 +13,36 @@ class Category(models.Model):
     """
     name = models.CharField(
         max_length=255,
-        verbose_name='Name',
+        verbose_name='Nazwa',
         null=False,
-        default='Name?',
         unique=True,
+    )
+
+
+class Institution(models.Model):
+    """
+    Class creates a model storing information regarding available institutions.
+    """
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Nazwa Instytucji',
+        null=False,
+        unique=True,
+    )
+    description = models.TextField(
+        verbose_name='Opis',
+        null=True,
+    )
+    type = models.CharField(
+        max_length=255,
+        choices=ORGANIZATIONS,
+        default=ORGANIZATIONS[0],
+        null=False,
+        verbose_name='Typ',
+    )
+    category = models.ManyToManyField(
+        to=Category,
+        verbose_name='Kategoria',
+        related_name='institution',
+        null=False,
     )
