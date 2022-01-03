0d4538bc7b9af0f49754f4fedce7526c77bb05ff
Imrie83 <czolgista83@gmail.com>
2022-01-03 17:34:43 +0100

    add views tests

--- donate_project/tests/tests.py
+++ donate_project/tests/tests.py
@@ -15,3 +15,23 @@ def test_category(client, create_categories):
 def test_institute(client, create_institute):
     assert Institution.objects.get(name='Inst 1')
     assert len(Institution.objects.all()) == 2
+
+
+def test_landing_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_login_view(client):
+    response = client.get('/login/')
+    assert response.status_code == 200
+
+
+def test_register_view(client):
+    response = client.get('/register/')
+    assert response.status_code == 200
+
+
+def test_donate_view(client):
+    response = client.get('/donate/')
+    assert response.status_code == 200
