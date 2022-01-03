fce3b9157c15b65be7f8ba8554d8521694f8a6a7
Imrie83 <czolgista83@gmail.com>
2022-01-03 13:07:38 +0100

    add Category model

--- donate/donate_app/models.py
+++ donate/donate_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Class creates a model storing Categories names.
+    """
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Name',
+        null=False,
+        default='Name?',
+        unique=True,
+    )
