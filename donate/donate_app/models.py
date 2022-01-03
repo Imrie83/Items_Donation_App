131106b8e934e10162f5a40c377f959d5c221019
Imrie83 <czolgista83@gmail.com>
2022-01-03 13:34:49 +0100

    add Donation model

--- donate/donate_app/models.py
+++ donate/donate_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 ORGANIZATIONS = [
@@ -44,5 +45,63 @@ class Institution(models.Model):
         to=Category,
         verbose_name='Kategoria',
         related_name='institution',
+    )
+
+
+class Donation(models.Model):
+    """
+    Class creates a model storing information regarding a donation.
+    """
+    quantity = models.IntegerField(
+        verbose_name='Ilość',
         null=False,
+    )
+    category = models.ManyToManyField(
+        to=Category,
+        verbose_name='Kategoria',
+    )
+    institution = models.ForeignKey(
+        to=Institution,
+        verbose_name='Instytucja',
+        name='donation',
+        null=False,
+        on_delete=models.SET(None),
+    )
+    address = models.CharField(
+        max_length=255,
+        verbose_name='Adres',
+        null=True,
+    )
+    phone_number = models.IntegerField(
+        verbose_name='Telefon',
+        null=True,
+    )
+    city = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name='Miasto',
+    )
+    zip_code = models.CharField(
+        max_length=10,
+        verbose_name='Kod pocztowy',
+        null=True,
+    )
+    pick_up_date = models.DateField(
+        verbose_name='Data odbioru',
+        null=False,
+    )
+    pick_up_time = models.TimeField(
+        verbose_name='Godzina odbioru',
+        null=False
+    )
+    pick_up_comment = models.TextField(
+        verbose_name='Komentarz',
+        null=True,
+    )
+    user = models.ForeignKey(
+        to=User,
+        null=True,
+        default=None,
+        verbose_name='Użytkownik',
+        on_delete=models.SET(None),
     )
