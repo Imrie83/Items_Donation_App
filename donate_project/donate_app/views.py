643f25b63c881638639e8d3b31264324f16d885d
Imrie83 <czolgista83@gmail.com>
2022-01-04 14:59:17 +0100

    add pagination components - needs finishing

--- donate_project/donate_app/views.py
+++ donate_project/donate_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
 
 from donate_app.models import (
     Institution,
@@ -14,15 +15,32 @@ class LandingPageView(View):
     def get(self, request):
 
         bag_count = 0
+        # count all organisations
         supported_org = Institution.objects.all().count()
 
+        # count all bags donated
         donations = Donation.objects.all()
         for donation in donations:
             bag_count += donation.quantity
 
-        foundations = Institution.objects.filter(type=1)
+        foundation = Institution.objects.filter(type=1)
+        # fun_paginator = Paginator(foundation_list, 1)
+
         organization = Institution.objects.filter(type=2)
+        # org_paginator = Paginator(organization_list, 1)
+
         local_collection = Institution.objects.filter(type=3)
+        # loc_paginator = Paginator(local_collection_list, 1)
+
+        # TODO: Pagination
+        # fun_page = request.GET.get('page')
+        # org_page = request.GET.get('page2')
+        # loc_page = request.GET.get('page3')
+        #
+        # foundations = fun_paginator.get_page(fun_page)
+        # organization = org_paginator.get_page(org_page)
+        # local_collection = loc_paginator.get_page(loc_page)
+
 
         return render(
             request,
@@ -30,7 +48,7 @@ class LandingPageView(View):
             context={
                 'supported': supported_org,
                 'bag_count': bag_count,
-                'foundation': foundations,
+                'foundation': foundation,
                 'organization': organization,
                 'local_collection': local_collection,
             },
