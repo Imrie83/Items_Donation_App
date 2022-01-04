b9317dae4cc127450e45714b55376e07960921aa
Imrie83 <czolgista83@gmail.com>
2022-01-04 10:34:03 +0100

    Add dynamic Institution display on main page

--- donate_project/donate_app/views.py
+++ donate_project/donate_app/views.py
@@ -15,9 +15,14 @@ class LandingPageView(View):
 
         bag_count = 0
         supported_org = Institution.objects.all().count()
+
         donations = Donation.objects.all()
         for donation in donations:
             bag_count += donation.quantity
+
+        foundations = Institution.objects.filter(type=1)
+        organization = Institution.objects.filter(type=2)
+        local_collection = Institution.objects.filter(type=3)
 
         return render(
             request,
@@ -25,6 +30,9 @@ class LandingPageView(View):
             context={
                 'supported': supported_org,
                 'bag_count': bag_count,
+                'foundation': foundations,
+                'organization': organization,
+                'local_collection': local_collection,
             },
         )
 
